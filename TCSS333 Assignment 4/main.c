e61071f77368a9e2e1e899f668b4876d493168ac
Alexander Terikov <teraliv@gmail.com>
2015-11-09 20:56:44 -0800

    Final commit. Project is done.

--- TCSS333 Assignment 4/main.c
+++ TCSS333 Assignment 4/main.c
@@ -2,7 +2,7 @@
  *  main.c
  *  TCSS 333 - Autumn 2015
  *
- *  Assignment 4 - Image Manipulation without Arrays
+ *  Assignment 4 - Image Manipulation Without Arrays
  *  Alex Terikov (teraliv@uw.edu)
  *  10/29/15
  */
@@ -12,69 +12,106 @@
 #include <string.h>
 #include <sys/stat.h>
 
-void imageDoubleExposure(unsigned char *img1, unsigned char *img2, int size);
-void imageCheckerBoard(unsigned char *image1, unsigned char *image2, int size);
-void readParameters(int num, char **param);
+
+int checkFilesAndMakeImages(const char *fname1, const char *fname2);
+void imageDoubleExposure(unsigned char *img1, unsigned char *img2, long size);
+void imageCheckerBoard(unsigned char *img1, unsigned char *img2, long size);
 unsigned long getFileSize(char *file);
-int compareFileExtensions(char *ext1, char *ext2);
 
 
 int main(int argc, const char **argv) {
-    
-//    readParameters(argc, (char **)argv);
-    
-    char **p = (char **) argv;
-    char *ext1 = *++p;
-    char *ext2 = *++p;
 
-    compareFileExtensions(ext1, ext2);
+    // get file names
+    const char *fname1 = *++argv;
+    const char *fname2 = *++argv;
+
     
-    FILE *bmpFile1 = fopen(*(argv+1), "rb");
-    FILE *bmpFile2 = fopen(*(argv+2), "rb");
-    
-    unsigned long fsize1 = 0; // 1st file size
-    unsigned long fsize2 = 0; // 2nd file size
-    
-    
-    // if both files exists and were open correctly
-    // then get their sizes
-    if (bmpFile1 && bmpFile2) {
-        fsize1 = getFileSize((char *) *++argv);
-        fsize2 = getFileSize((char *) *++argv);
-    }
-    
-//    printf("%s\n", *argv);
-    
-    // print out error message if file not opened
-    if (!bmpFile1 || !bmpFile2 || (fsize1 != fsize2) || !compareFileExtensions(ext1, ext2)) {
-        printf("File does not exist or files are with different sizes!");
+    // check if anything wrong with a file or bitmap data
+    if (!checkFilesAndMakeImages(fname1, fname2)) {
+        printf("File does not exist or incorrect!\n");
         exit(0);
     }
     
-    
-    unsigned char *image1;
-    unsigned char *image2;
-    
-    image1 = malloc(fsize1);
-    image2 = malloc(fsize2);
-    
-    fread(image1, sizeof(char), 499446, bmpFile1);
-    fread(image2, sizeof(char), 499446, bmpFile2);
-    
-    imageDoubleExposure(image1, image2, (int) fsize1);
-    imageCheckerBoard(image1, image2, (int) fsize1);
-    
-    free(image1);
-    free(image2);
-    
-    fclose(bmpFile1);
-    fclose(bmpFile2);
+    // if both files are good make image manipulations
+    // and create two new BMP images
+    else {
+        checkFilesAndMakeImages(fname1, fname2);
+    }
     
     return 0;
 }
 
 
-void imageDoubleExposure(unsigned char *img1, unsigned char *img2, int size) {
+/*
+ * This function check if both files were open correctly
+ * and checks if the data is correct for a bitmap image file.
+ *
+ * @param *fname1 - file name for the first image
+ * @param *fname2 - file name for the second image
+ * @return - returns 0 if the file is missing ot incorrect, otherwise 1.
+ */
+int checkFilesAndMakeImages(const char *fname1, const char *fname2) {
+    
+    // open BMP files in a binary mode
+    FILE *bmpFile1 = fopen(fname1, "rb");
+    FILE *bmpFile2 = fopen(fname2, "rb");
+    
+    int result = 0;
+    
+    unsigned char *image1;
+    unsigned char *image2;
+    
+    unsigned long fsize1; // 1st file size
+    unsigned long fsize2; // 2nd file size
+    
+    // check if both files were open
+    if (bmpFile1 && bmpFile2) {
+        
+        // get the sizes of both files
+        fsize1 = getFileSize((char *) fname1);
+        fsize2 = getFileSize((char *) fname2);
+        
+        // compare if the file sizes are the same
+        if (fsize1 == fsize2) {
+            image1 = malloc(fsize1);
+            image2 = malloc(fsize2);
+            
+            unsigned char *p1 = image1;
+            unsigned char *p2 = image2;
+            
+            fread(image1, sizeof(char), fsize1, bmpFile1);
+            fread(image2, sizeof(char), fsize2, bmpFile2);
+            
+            // Here we check BMP data by comparing first 2 byts:
+            // 1st byte: B
+            // 2nd byte: M
+            if (*p1 == 'B' && *p2 == 'B' && *++p1 == 'M' && *++p2 == 'M') {
+                imageDoubleExposure(image1, image2, fsize1);
+                imageCheckerBoard(image1, image2, fsize2);
+                
+                result = 1;
+            }
+            
+            free(image1);
+            free(image2);
+        }
+    }
+    
+    fclose(bmpFile1);
+    fclose(bmpFile2);
+    
+    return result;
+}
+
+
+/*
+ * Creates a bitmap image file with double exposure
+ * effect based on two images.
+ *
+ * @param *image1 - pointer to the 1st image array
+ * @param *image2 - pointer to the 2nd image array
+ */
+void imageDoubleExposure(unsigned char *img1, unsigned char *img2, long size) {
     
     // file for a double exposure image
     FILE *doubleExposureFile = fopen("blend.bmp", "wb");
@@ -95,14 +132,21 @@ void imageDoubleExposure(unsigned char *img1, unsigned char *img2, int size) {
     }
     
     // write bmp double exposure data to the file
-    fwrite(image, sizeof(unsigned char), size, doubleExposureFile);
-
+    fwrite(image, sizeof(char), size, doubleExposureFile);
     
     free(image);
     fclose(doubleExposureFile);
 }
 
-void imageCheckerBoard(unsigned char *img1, unsigned char *img2, int size) {
+
+/*
+ * Creates a bitmap image file with checker board
+ * effect based on two images.
+ *
+ * @param *image1 - pointer to the 1st image array
+ * @param *image2 - pointer to the 2nd image array
+ */
+void imageCheckerBoard(unsigned char *img1, unsigned char *img2, long size) {
     
     int width = *(img1 + 18) + (*(img1 + 19) * 256);
     int block = (width * 3) / 8;
@@ -130,7 +174,6 @@ void imageCheckerBoard(unsigned char *img1, unsigned char *img2, int size) {
                     count++;
                     i++;
                 }
-            
             } else {
                 while (i < (block)) {
                     *(image + count) = *(img2 + count);
@@ -140,53 +183,20 @@ void imageCheckerBoard(unsigned char *img1, unsigned char *img2, int size) {
             }
         }
     }
-
-    fwrite(image, sizeof(unsigned char), size, checkerBoardFile);
+    
+    // write bmp checker board data to the file
+    fwrite(image, sizeof(char), size, checkerBoardFile);
     
     free(image);
     fclose(checkerBoardFile);
 }
 
-int compareFileExtensions(char *ext1, char *ext2) {
-
-    int len1 = (int) strlen(ext1) - 1;
-    int len2 = (int) strlen(ext2) - 1;
-    int count = 0;
-    int result = 0;
-    int contin = 1;
-    
-    ext1 += len1;
-    ext2 += len2;
-    
-    while (contin && count < 4) {
-        if (*ext1 == *ext2)
-            result = 1;
-        
-        if (*ext1 != *ext2) {
-            result = 0;
-            contin = 0;
-        }
-        
-        count++;
-        ext1--;
-        ext2--;
-    }
-    
-    return result;
-}
-
-void readParameters(int total, char **param) {
-    int i;
-    
-    for (i = 1; i < 3; i++) {
-        printf("arg %i - %s\n", i, *(param+i));
-    }
-//    int i = total;
-//    do {
-//        printf("Argumment %d: %s\n", total - i, *param++);
-//    } while (--i);
-}
-
+/*
+ * Gets the size of the file.
+ *
+ * @param *file - the file to calculate the size.
+ * @return - returns the file size, or -1 if the file is bad.
+ */
 unsigned long getFileSize(char *file) {
     long result = -1;
     struct stat file_info;
